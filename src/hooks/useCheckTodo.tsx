8367a5365407debfedb4887c5ef5bbd169fbc556
Your Name <your-email@domain.com>
2023-07-04 21:57:04 +0545

    add optimistic update

--- src/hooks/useCheckTodo.tsx
+++ src/hooks/useCheckTodo.tsx
@@ -4,6 +4,7 @@ import { getTodoKeys } from "./useTodos";
 
 interface ICheckTodo {
   id: string;
+  completed: number;
 }
 export const checkTodo = async (checkTodo: ICheckTodo) => {
   const response = await API.put(`/todos/check/${checkTodo.id}`);
@@ -12,8 +13,38 @@ export const checkTodo = async (checkTodo: ICheckTodo) => {
 
 const useCheckTodo = () => {
   const queryClient = useQueryClient();
+  console.log(queryClient.getQueryData(getTodoKeys.all));
   return useMutation({
     mutationFn: checkTodo,
+    onMutate: async (newTodo) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey: getTodoKeys.all });
+
+      // Snapshot the previous value
+      const previousTodos: ICheckTodo[] =
+        queryClient.getQueryData(getTodoKeys.all) || [];
+
+      const val = previousTodos.map((a: ICheckTodo) => {
+        if (Number(a.id) === Number(newTodo.id)) {
+          return {
+            ...a,
+            completed: 1,
+          };
+        } else {
+          return a;
+        }
+      });
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(getTodoKeys.all, () => val);
+
+      // Return a context object with the snapshotted value
+      return { previousTodos };
+    },
+    onError: (err, newTodo, context) => {
+      queryClient.setQueryData(getTodoKeys.all, context?.previousTodos);
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: getTodoKeys.all });
     },
--- src/hooks/useUnCheckTodo.tsx
+++ src/hooks/useUnCheckTodo.tsx
@@ -4,6 +4,7 @@ import { getTodoKeys } from "./useTodos";
 
 interface IUnCheckTodo {
   id: string;
+  completed: number;
 }
 export const unCheckTodo = async (UnCheckTodo: IUnCheckTodo) => {
   const response = await API.put(`/todos/uncheck/${UnCheckTodo.id}`);
@@ -14,6 +15,35 @@ const useUnCheckTodo = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: unCheckTodo,
+    onMutate: async (newTodo) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey: getTodoKeys.all });
+
+      // Snapshot the previous value
+      const previousTodos: IUnCheckTodo[] =
+        queryClient.getQueryData(getTodoKeys.all) || [];
+
+      const val = previousTodos.map((a: IUnCheckTodo) => {
+        if (Number(a.id) === Number(newTodo.id)) {
+          return {
+            ...a,
+            completed: 0,
+          };
+        } else {
+          return a;
+        }
+      });
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(getTodoKeys.all, () => val);
+
+      // Return a context object with the snapshotted value
+      return { previousTodos };
+    },
+    onError: (err, newTodo, context) => {
+      queryClient.setQueryData(getTodoKeys.all, context?.previousTodos);
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: getTodoKeys.all });
     },
