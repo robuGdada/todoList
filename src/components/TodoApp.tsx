028455d5e271e683f633752dd620d9a146f6c48b
Your Name <your-email@domain.com>
2023-07-04 13:31:43 +0545

    add checkbox fn

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,19 +1,21 @@
 import TodoInfo from "@/components/TodoInfo";
 import { useTodos } from "@/hooks/useTodos";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 
 const TodoApp = () => {
   const router = useRouter();
   const { data } = useTodos();
   return (
     <>
-      <header>
+      <header className="header">
         <button className="btn-login">Login</button>
       </header>
-      <button className="btn-add" onClick={() => router.push("/create-todo")}>
-        Add a todo
-      </button>
+      <div className="btn">
+        <button className="btn-add" onClick={() => router.push("/create-todo")}>
+          Add a todo
+        </button>
+      </div>
       <div className="todo-info">
         {data && data.map((todo) => <TodoInfo key={todo.id} todo={todo} />)}
       </div>
--- src/components/TodoInfo.tsx
+++ src/components/TodoInfo.tsx
@@ -1,5 +1,11 @@
+import { useCheckTodo } from "@/hooks/useCheckTodo";
+import { useTodoDelete } from "@/hooks/useTodoDelete";
+import { useUnCheckTodo } from "@/hooks/useUnCheckTodo";
 import { TTodo } from "@/types/TTodo";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { AiOutlineEdit } from "react-icons/Ai";
+import { AiOutlineDelete } from "react-icons/Ai";
 
 interface TodoInfoProps {
   todo: TTodo | undefined;
@@ -9,11 +15,56 @@ const TodoInfo = ({ todo }: TodoInfoProps) => {
   if (!todo) {
     return null;
   }
+  const mutation = useTodoDelete();
+  const { push } = useRouter();
+  const checkMutation = useCheckTodo();
+  const UnCheckMutation = useUnCheckTodo();
+
+  const handleOnChange = () => {
+    if (todo.completed === 1) {
+      checkMutation.mutate({ id: String(todo.id) });
+    } else {
+      UnCheckMutation.mutate({ id: String(todo.id) });
+    }
+  };
 
   return (
     <>
-      <div key={todo.id}>
-        <h3>{todo.title}</h3>
+      <div className="info-container">
+        <div key={todo.id} className="todo-box">
+          <div className="horizontal-container">
+            <input
+              className="checkbox"
+              type="checkbox"
+              value={todo.title}
+              onChange={handleOnChange}
+            />
+            <h3 className="todo-title">{todo.title}</h3>
+          </div>
+          <div className="icon-container">
+            <div
+              className="icon-wrapper"
+              onClick={() => {
+                push({
+                  pathname: "/edit-todo",
+                  query: {
+                    id: todo.id,
+                    title: todo.title,
+                    description: todo.description,
+                  },
+                });
+              }}
+            >
+              <AiOutlineEdit className="edit-icon" />
+            </div>
+            <div
+              className="icon-wrapper"
+              onClick={() => mutation.mutate({ id: String(todo.id) })}
+            >
+              <AiOutlineDelete className="delete-icon" />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
