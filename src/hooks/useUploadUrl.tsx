70bd8d2bfcbabceb0e191b03ed0f378eced858b7
Your Name <your-email@domain.com>
2023-07-10 12:58:41 +0545

    mutation hook for upload to s3

---
+++ src/hooks/useUploadUrl.tsx
@@ -0,0 +1,25 @@
+import { API } from "@/api/API";
+import { MutationFunction, useMutation } from "@tanstack/react-query";
+
+interface S3Upload {
+  uploadUrl?: string;
+  url?: string;
+}
+export const getUploadUrlKeys = {
+  all: ["getUploadUrl"] as const,
+};
+
+const fetchUploadUrlData: MutationFunction<S3Upload> = async () => {
+  const response = await API.post("/s3_upload_url");
+  return response.data;
+};
+
+const useUploadUrl = ({ ...rest }) => {
+  return useMutation({
+    mutationFn: fetchUploadUrlData,
+    mutationKey: getUploadUrlKeys.all,
+    ...rest,
+  });
+};
+
+export { useUploadUrl };
