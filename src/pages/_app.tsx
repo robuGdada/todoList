a3729b3e51c05778b33609104060fdfc905ef9d9
Your Name <your-email@domain.com>
2023-07-10 12:58:50 +0545

    add some css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../components/TodoApp.css";
+import "./auth/login.css";
+import "./registration/registration.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
