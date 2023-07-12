b814816067bb62ffdd9e712b0f09bf1b2f2e3346
Your Name <your-email@domain.com>
2023-07-12 10:13:22 +0545

    query hook for category

---
+++ src/pages/create-todo/useCategoryQuery.tsx
@@ -0,0 +1,28 @@
+import { API } from "@/api/API";
+import { QueryFunction, useQuery } from "@tanstack/react-query";
+
+interface Tcategory {
+  id?: string;
+  name: String;
+}
+
+export const getCategoryKeys = {
+  all: ["getCategory"] as const,
+};
+
+type TGetCategoryKey = typeof getCategoryKeys.all;
+const fetchCategoryData: QueryFunction<
+  Tcategory[],
+  TGetCategoryKey
+> = async () => {
+  const response = await API.get("category");
+  return response.data;
+};
+
+const useCategoryQuery = () => {
+  return useQuery({
+    queryKey: getCategoryKeys.all,
+    queryFn: fetchCategoryData,
+  });
+};
+export { useCategoryQuery };
