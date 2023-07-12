a8f879a0c6d895b7259e43e60dd10391be573eae
Your Name <your-email@domain.com>
2023-07-12 10:13:48 +0545

    add category

--- src/pages/edit-todo/index.tsx
+++ src/pages/edit-todo/index.tsx
@@ -1,10 +1,11 @@
-import { useTodoUpdate } from "@/hooks/useTodoUpdate";
-import { useUploadUrl } from "@/hooks/useUploadUrl";
 import { fileToBlob } from "@/utils/fileToBlob";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { useRouter } from "next/router";
 import React, { ChangeEvent, useState } from "react";
+import { useCategoryQuery } from "../create-todo/useCategoryQuery";
+import { useTodoUpdate } from "@/hooks/useTodoUpdate";
+import { useUploadUrl } from "@/hooks/useUploadUrl";
 
 type UploadResponse = {
   message: string;
@@ -15,9 +16,11 @@ const EditTodo = () => {
   const { query } = useRouter();
   const [title, setTitle] = useState(query.title);
   const [description, setDescription] = useState(query.description);
-  const [file, setFile] = useState<any>(null);
+  const [file, setFile] = useState<any>(query.imageUrl || null);
   const [fileType, setFileType] = useState("");
+  const [categoryId, setCategoryId] = useState(query.categoryId);
 
+  const { data: categoryData } = useCategoryQuery();
   const { mutateAsync: editTodo } = useTodoUpdate();
 
   const uploadToS3 = async (val: { uploadUrl: string; blobData: Blob }) => {
@@ -47,15 +50,26 @@ const EditTodo = () => {
 
   const { mutate: updateTodoWithImage } = useUploadUrl({
     onSuccess: async (res: UploadResponse) => {
-      const blobData = await fileToBlob(file, fileType);
-      await s3Mutate({ uploadUrl: res.data.uploadUrl, blobData: blobData });
-      await editTodo({
-        id: query.id as string,
-        title: String(title),
-        description: String(description),
-        imageUrl: res.data.url || (query.imageUrl as string),
-      });
+      if (file) {
+        const blobData = await fileToBlob(file, fileType);
+        await s3Mutate({ uploadUrl: res.data.uploadUrl, blobData: blobData });
+        await editTodo({
+          id: query.id as string,
+          title: String(title),
+          description: String(description),
+          categoryId: Number(categoryId),
+          imageUrl: res.data.url || (query.imageUrl as string),
+        });
+      } else {
+        await editTodo({
+          id: query.id as string,
+          title: String(title),
+          description: String(description),
+          categoryId: Number(categoryId),
+        });
+      }
     },
+
     onError: (error: any) => {
       console.log(error);
     },
@@ -63,6 +77,11 @@ const EditTodo = () => {
 
   const handleOnSubmit = () => {
     updateTodoWithImage({});
+  };
+
+  const handleImageDeselect = () => {
+    setFile(null);
+    setFileType("");
   };
 
   return (
@@ -74,15 +93,42 @@ const EditTodo = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+        <div className="error-message">
+          {!title ? <div>Title required</div> : ""}
+        </div>
         <textarea
           className="text-area"
           placeholder="Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
+        <div className="error-message">
+          {!description ? <div>Description required</div> : ""}
+        </div>
         <div className="App">
           <input type="file" onChange={handleImageSelect} />
-          <img src={file} />
+          {file && (
+            <div>
+              <img src={file} alt="Selected Image" />
+              <button onClick={handleImageDeselect}>X</button>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <select
+            value={categoryId}
+            onChange={(e) => {
+              setCategoryId(e.target.value);
+            }}
+          >
+            {categoryData &&
+              categoryData.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
+          </select>
         </div>
         <button className="btn-submit" onClick={handleOnSubmit}>
           Submit
